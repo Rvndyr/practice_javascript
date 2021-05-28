2c959912ab7cee8a9758e3454b8f54a24877a234
Randy Rodriguez <Randyr2125@msn.com>
2021-05-27 20:37:31 -0400

    added axios route to get todos

--- practice_vue/index.js
+++ practice_vue/index.js
@@ -5,12 +5,23 @@ var app = new Vue({
     return {
       message: "Hello from Vue.js!",
       name: "Randy",
+      todos: [],
     };
+  },
+  created: function () {
+    this.loadTodos();
   },
   methods: {
     changeText: function () {
       console.log("Changing text");
       this.message = "New Message!!";
+    },
+    loadTodos: function () {
+      axios.get("https://jsonplaceholder.typicode.com/todos").then((response) => {
+        console.log("First: ", response.data);
+
+        this.todos = response.data;
+      });
     },
   },
 });
